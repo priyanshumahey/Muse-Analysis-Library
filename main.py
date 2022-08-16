0248afb73343d823cba3bd8be9b88973e74a9385
priyanshumahey <72003945+priyanshumahey@users.noreply.github.com>
2022-08-16 10:25:51 -0700

    Updated the library

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from muse_an_lib import Validate
 from muse_an_lib import RandomData
 from muse_an_lib import Visualize
+from muse_an_lib import Filter 
+import pandas as pd
+import numpy as np
+from scipy import fftpack
 dataset = [[1654046931.830125,[18.5546875, 27.34375, 121.09375, 37.59765625, -43.45703125]],
     [1654046931.8340313,[36.62109375, 30.2734375, 146.97265625, 41.015625, -41.9921875]],
     [1654046931.8379376,[33.203125, 29.78515625, 21.97265625, 30.2734375, 33.203125]],
@@ -13,8 +17,27 @@ print(Validate.TimeStamp(dataset))
 print(Validate.Values(dataset))
 print(Validate.ValuesSpecific(dataset, [0, 4]))
 
-print(RandomData.sig_gen(3, 2, 80, 50, 3))
+#print(RandomData.sig_gen(3, 2, 80, 50, 3))
+
+def fft(signal_noise, sec):
+    time = np.linspace(0, sec, 1000, endpoint=True)
+    sig_noise_fft = fftpack.fft(signal_noise)
+    sig_noise_amp = 2 / time.size * np.abs(sig_noise_fft)
+    sig_noise_freq = np.abs(fftpack.fftfreq(time.size, 3/1000))
+    return (sig_noise_fft, sig_noise_amp, sig_noise_freq)
+
+def amp_calc(sig_noise_amp):
+    signal_amplitude = pd.Series(sig_noise_amp).nlargest(2).round(0).astype(int).tolist()
+    return signal_amplitude
+
+
+x = Filter.butter_highpass_filter(RandomData.sig_gen(3, 2, 80, 50, 3), 10, 30)
+print(amp_calc(x[1]))
+
+
+
+
 
 import matplotlib.pyplot as plt
-plt.plot(dataset[0][1])
+plt.plot(x)
 plt.show()
